1d8cfb1df536712b04144e51742157ef133dd653
Timothy Gerla <tim@gerla.net>
2023-10-26 07:45:37 -0400

    implement button handling and add fonts
    Signed-off-by: Timothy Gerla <tim@gerla.net>

--- drummer.py
+++ drummer.py
@@ -5,44 +5,48 @@ import pygame
 
 from display import ST7735RDisplay
 from drum_machine import DrumMachine
+from gpiozero import Button
 
-def handle_events(drum_machine, display):
+class Controller:
+    def __init__(self):
+        self.jUp = Button(6)
+        self.jDown = Button(19)
+        self.jLeft = Button(5)
+        self.jRight = Button(26)
+        self.jPress = Button(13)
+        self.key1 = Button(21)
+        self.key2 = Button(20)
+        self.key3 = Button(16)
+
+def handle_events(drum_machine, display, controller):
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key in (pygame.K_ESCAPE, pygame.K_q)):
+        if event.type == pygame.QUIT:
             drum_machine.stop()
             return False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            if drum_machine.state == "playing":
-                drum_machine.stop()
-            else:
-                drum_machine.start()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            if drum_machine.current_pattern_idx < len(drum_machine.pattern_loader.patterns["patterns"]):
-                drum_machine.current_pattern_idx += 1
-            else:
-                drum_machine.current_pattern_idx = 1
-            drum_machine.switch_pattern(drum_machine.current_pattern_idx)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            if drum_machine.current_pattern_idx > 1:
-                drum_machine.current_pattern_idx -= 1
-            else :
-                drum_machine.current_pattern_idx = len(drum_machine.pattern_loader.patterns["patterns"])
-            drum_machine.switch_pattern(drum_machine.current_pattern_idx)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            drum_machine.set_tempo(drum_machine.tempo + 1)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            drum_machine.set_tempo(drum_machine.tempo - 1)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            drum_machine.switch_measure("A")
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_b:
-            drum_machine.switch_measure("B")
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_t:
-            if "T" in drum_machine._current_pattern["measures"]:
-                drum_machine.switch_measure("T")
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
-            display.capture_screenshot()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_TAB:
-            drum_machine.tap_tempo()
+        
+    
+    if controller.key3.is_pressed:
+        if drum_machine.state == "playing":
+            drum_machine.stop()
+        else:
+            drum_machine.start()
+    if controller.jRight.is_pressed:
+        if drum_machine.current_pattern_idx < len(drum_machine.pattern_loader.patterns["patterns"]):
+            drum_machine.current_pattern_idx += 1
+        else:
+            drum_machine.current_pattern_idx = 1
+        drum_machine.switch_pattern(drum_machine.current_pattern_idx)
+    if controller.jLeft.is_pressed:
+        if drum_machine.current_pattern_idx > 1:
+            drum_machine.current_pattern_idx -= 1
+        else :
+            drum_machine.current_pattern_idx = len(drum_machine.pattern_loader.patterns["patterns"])
+        drum_machine.switch_pattern(drum_machine.current_pattern_idx)
+    if controller.jUp.is_pressed:
+        drum_machine.set_tempo(drum_machine.tempo + 1)
+    if controller.jDown.is_pressed:
+        drum_machine.set_tempo(drum_machine.tempo - 1)
+
     return True
 
 def main():
@@ -52,6 +56,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     display = ST7735RDisplay()
+    controller = Controller()
 
     drum_machine = DrumMachine()
     drum_machine.load_patterns()
@@ -61,7 +66,7 @@ def main():
         delta_time = clock.tick(60)
         drum_machine.loop(delta_time)
         display.loop(drum_machine)
-        running = handle_events(drum_machine, display)
+        running = handle_events(drum_machine, display, controller)
         
     pygame.quit()
 
