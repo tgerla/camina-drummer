1213d0984907179e33e42f45c58eb32a87df4f61
Timothy Gerla <tim@gerla.net>
2023-10-30 12:38:54 -0400

    lint
    Signed-off-by: Timothy Gerla <tim@gerla.net>

--- display.py
+++ display.py
@@ -13,13 +13,13 @@ else:
 
 from PIL import Image, ImageDraw, ImageFont
 
-SCREEN_WIDTH = 240 
+SCREEN_WIDTH = 240
 SCREEN_HEIGHT = 240
+
 
 # implement an abstract display class that will be used by the main loop
 class Display:
     def __init__(self):
-
         self._setup_fonts()
         self._setup_display()
 
@@ -31,7 +31,7 @@ class Display:
         raise NotImplementedError
 
     def _setup_fonts(self):
-        self.regularFont = ImageFont.truetype("fonts/NotoSans-Regular.ttf", 24) 
+        self.regularFont = ImageFont.truetype("fonts/NotoSans-Regular.ttf", 24)
         self.boldFont = ImageFont.truetype("fonts/NotoSans-Bold.ttf", 32)
         self.smallBoldFont = ImageFont.truetype("fonts/NotoSans-Bold.ttf", 20)
 
@@ -46,24 +46,44 @@ class Display:
         self._draw_tempo_display(drum_machine, d)
         self._draw_pattern_display(drum_machine, d)
         self._draw_measures(drum_machine, d)
-#        self._draw_beats(drum_machine, d)
+        #        self._draw_beats(drum_machine, d)
         self.flip(None)
 
     def _draw_pattern_name(self, drum_machine, d):
-        d.text((10, 160), drum_machine.get_current_pattern_name(), font=self.regularFont, fill=(255, 255, 255))
+        d.text(
+            (10, 140),
+            drum_machine.get_current_pattern_name(),
+            font=self.smallBoldFont,
+            fill=(255, 255, 255),
+        )
 
     def _draw_tempo_display(self, drum_machine, d):
-        d.text((170, 18), "%d" % drum_machine.tempo, font=self.regularFont, fill=(255, 255, 255))
+        d.text(
+            (170, 18),
+            "%d" % drum_machine.tempo,
+            font=self.regularFont,
+            fill=(255, 255, 255),
+        )
 
         if not drum_machine.beat % 4 and drum_machine.state == "playing":
             tsColor = (255, 255, 0)
         else:
             tsColor = (128, 128, 128)
 
-        d.text((SCREEN_WIDTH/2-8, 30), "%s" % drum_machine.pattern_signature, font=self.regularFont, fill=tsColor)
+        d.text(
+            (SCREEN_WIDTH / 2 - 8, 30),
+            "%s" % drum_machine.pattern_signature,
+            font=self.regularFont,
+            fill=tsColor,
+        )
 
     def _draw_pattern_display(self, drum_machine, d):
-        d.text((180, 170), "%d" % drum_machine.current_pattern_idx, font=self.boldFont, fill=(255, 255, 255))
+        d.text(
+            (160, 190),
+            "%d" % drum_machine.current_pattern_idx,
+            font=self.boldFont,
+            fill=(255, 255, 255),
+        )
 
     def _draw_measures(self, drum_machine, d):
         for i, m in enumerate(drum_machine._current_pattern["measures"]):
@@ -74,7 +94,7 @@ class Display:
             else:
                 color = (128, 128, 128)
 
-            d.text((i*22+19, 85), "%s" % m, font=self.smallBoldFont, fill=color)
+            d.text((i * 22 + 17, 189), "%s" % m, font=self.smallBoldFont, fill=color)
 
     def _draw_beats(self, drum_machine, d):
         for i in range(0, drum_machine.pattern_length):
@@ -83,7 +103,7 @@ class Display:
             bar_height = 20
             bar_y_position = SCREEN_HEIGHT - bar_height - 10
 
-            x = 8 + i * (bar_width+bar_spacing)
+            x = 8 + i * (bar_width + bar_spacing)
             y = bar_y_position
             x2 = x + bar_width
             y2 = y + bar_height
@@ -91,12 +111,11 @@ class Display:
 
             if i == drum_machine.beat:
                 d.rectangle([x, y, x2, y2], outline=(0, 0, 0), fill=(0, 255, 255))
-            
 
         if not drum_machine.beat % 4 and drum_machine.state == "playing":
-            x1 = SCREEN_WIDTH/2 - 4
+            x1 = SCREEN_WIDTH / 2 - 4
             y1 = 32 - 4
-            x2 = SCREEN_WIDTH/2 + 4
+            x2 = SCREEN_WIDTH / 2 + 4
             y2 = 32 + 4
             d.ellipse([x1, y1, x2, y2], outline=(255, 255, 0), fill=(255, 255, 0))
 
@@ -107,17 +126,17 @@ class ST7735RDisplay(Display):
 
     def _setup_display(self):
         self.display = ST7789.ST7789(
-            height = SCREEN_HEIGHT,
-            width = SCREEN_WIDTH,
+            height=SCREEN_HEIGHT,
+            width=SCREEN_WIDTH,
             rotation=0,
             port=0,
             cs=0,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
             dc=25,
             rst=27,
-            backlight=24,               # 18 for back BG slot, 19 for front BG slot.
+            backlight=24,  # 18 for back BG slot, 19 for front BG slot.
             spi_speed_hz=80 * 1000 * 1000,
             offset_left=0,
-            offset_top=0
+            offset_top=0,
         )
         self.display.begin()
 
@@ -125,18 +144,24 @@ class ST7735RDisplay(Display):
         self.display.display(drawingContext)
 
 
+SIM_WIDTH = 595
+SIM_HEIGHT = 280
+
 
 class PyGameDisplay(Display):
     screen = None
 
     def _setup_display(self):
         print("pygame display")
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SIM_WIDTH, SIM_HEIGHT))
         pygame.display.set_caption("Camina Drummer")
+        self.screen.blit(pygame.image.load("assets/sim-background.png"), (0, 0))
 
     def flip(self, drawingContext):
-        img = pygame.image.frombuffer(self.buffer.tobytes(), self.buffer.size, self.buffer.mode)
-        self.screen.blit(img, (0, 0))
+        img = pygame.image.frombuffer(
+            self.buffer.tobytes(), self.buffer.size, self.buffer.mode
+        )
+        self.screen.blit(img, (175, 20))
         pygame.display.flip()
 
     def capture_screenshot(self):
--- drummer.py
+++ drummer.py
@@ -11,6 +11,7 @@ try:
 except ImportError:
     print("gpiozero not found")
 
+
 class GpioZeroController:
     def __init__(self):
         self.jUp = Button(6)
@@ -22,20 +23,22 @@ class GpioZeroController:
         self.key2 = Button(20)
         self.key3 = Button(16)
 
+
 def handle_events(drum_machine, display, controller):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drum_machine.stop()
             return False
-        
-    
+
     if controller.key3.is_pressed:
         if drum_machine.state == "playing":
             drum_machine.stop()
         else:
             drum_machine.start()
     if controller.jRight.is_pressed:
-        if drum_machine.current_pattern_idx < len(drum_machine.pattern_loader.patterns["patterns"]):
+        if drum_machine.current_pattern_idx < len(
+            drum_machine.pattern_loader.patterns["patterns"]
+        ):
             drum_machine.current_pattern_idx += 1
         else:
             drum_machine.current_pattern_idx = 1
@@ -43,8 +46,10 @@ def handle_events(drum_machine, display, controller):
     if controller.jLeft.is_pressed:
         if drum_machine.current_pattern_idx > 1:
             drum_machine.current_pattern_idx -= 1
-        else :
-            drum_machine.current_pattern_idx = len(drum_machine.pattern_loader.patterns["patterns"])
+        else:
+            drum_machine.current_pattern_idx = len(
+                drum_machine.pattern_loader.patterns["patterns"]
+            )
         drum_machine.switch_pattern(drum_machine.current_pattern_idx)
     if controller.jUp.is_pressed:
         drum_machine.set_tempo(drum_machine.tempo + 1)
@@ -52,6 +57,7 @@ def handle_events(drum_machine, display, controller):
         drum_machine.set_tempo(drum_machine.tempo - 1)
 
     return True
+
 
 def main():
     running = True
@@ -69,7 +75,7 @@ def main():
     drum_machine = DrumMachine()
     drum_machine.load_patterns()
     drum_machine.start()
-    
+
     print("%r" % display)
     while running:
         delta_time = clock.tick(60)
@@ -84,8 +90,8 @@ def main():
                 if event.type == pygame.QUIT:
                     running = False
 
-
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
